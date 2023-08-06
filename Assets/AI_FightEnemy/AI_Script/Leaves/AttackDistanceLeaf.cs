f1ffb61c049ec1448a1f69544bbef2494d6b261c
Nicolò Lombino <lombino97@gmail.com>
2023-08-06 23:26:45 +0200

    improve fight system

--- Assets/AI_FightEnemy/AI_Script/Leaves/AttackDistanceLeaf.cs
+++ Assets/AI_FightEnemy/AI_Script/Leaves/AttackDistanceLeaf.cs
@@ -14,6 +14,8 @@ public class AttackDistanceLeaf : Leaf
     [SerializeField]
     private Transform attackDistanceHole;
 
+    public GameObject[] colliders;
+
     public async override Task<Outcome> Run(GameObject agent, Dictionary<string, object> blackboard)
     {
         if (!agent.TryGetComponent<NavMeshAgent>(out var navMeshAgent))
@@ -21,13 +23,23 @@ public class AttackDistanceLeaf : Leaf
             return Outcome.FAIL;
         }
         
+
         navMeshAgent.isStopped = true;
         float waitTime = 0;
         waitTime = agent.GetComponent<EnemyMonster>().hasRageMode ? 1.5f : 3;
         agent.GetComponent<EnemyMonster>().LookAtTarget();
         agent.GetComponent<Animator>().SetTrigger("Jumping");
-        await Task.Delay((int)(waitTime * 1000));
+        await Task.Delay((int)((waitTime / 2) * 1000));
+        foreach (GameObject col in colliders)
+        {
+            col.SetActive(true);
+        }
+        await Task.Delay((int)((waitTime / 2) * 1000));
         GameObject attackDistance = Instantiate(attackDistancePrefab, attackDistanceHole.position, attackDistanceHole.rotation);
+        foreach (GameObject col in colliders)
+        {
+            col.SetActive(false);
+        }
         await Task.Delay((int)((waitTime + 2) * 1000));
         return Outcome.SUCCESS;
     }
